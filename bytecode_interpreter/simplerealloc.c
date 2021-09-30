db07f800fa2c964c2b981d8697f8519db34e33e2
Mark Jenkins <mark@markjenkins.ca>
2021-09-30 00:12:18 -0500

    copyright/license uniformity fix

--- bytecode_interpreter/simplerealloc.c
+++ bytecode_interpreter/simplerealloc.c
@@ -1,4 +1,4 @@
-/*  Copyright (c) 2021 Mark Jenkins
+/*  Copyright (c) 2021 Mark Jenkins <mark@markjenkins.ca>
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to
