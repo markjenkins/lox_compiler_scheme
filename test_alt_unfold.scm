cb565d67adb4b44fe59a5660f2e40db1e843b8da
Mark Jenkins <mark@markjenkins.ca>
2021-09-30 00:53:19 -0500

    single ; comments replaced with ;;; for top level scope

--- test_alt_unfold.scm
+++ test_alt_unfold.scm
@@ -1,5 +1,5 @@
-;(define (testf iteritem remainingiter)
-;  (cons iteritem remainingiter) )
+;;;(define (testf iteritem remainingiter)
+;;;  (cons iteritem remainingiter) )
 (define testf cons)
 
 (display (alt_unfold_pairtest_p testf (list 1 2 3 4)) )
