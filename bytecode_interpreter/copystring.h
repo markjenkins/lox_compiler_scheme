6f9af600337b26ee82c24eceb281a5bce47e26ba
Mark Jenkins <mark@markjenkins.ca>
2021-10-16 13:01:09 -0500

    copyright headers

--- bytecode_interpreter/copystring.h
+++ bytecode_interpreter/copystring.h
@@ -1,2 +1,28 @@
+/*  A derivitive work of https://craftinginterpreters.com
+
+    Copyright (c) 2015 Robert Nystrom
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to
+    deal in the Software without restriction, including without limitation the
+    rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+    sell copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+
+    Ported to M2-Planet C variant by
+    @author Mark Jenkins <mark@markjenkins.ca>
+*/
+
 ObjString* takeString(char* chars, int length, VM * vm);
 ObjString* copyString(char* chars, int length, VM * vm);
